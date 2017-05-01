0c9d761b838b5a66c367f075f5e643882f68404a
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2017-05-01 11:29:38 +1000

    Restrict users from modifying models

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,5 +1,7 @@
 module CoursesHelper
 	def course_editable?(course)
-		course.created_by.id == current_user.id
+		if current_user && course.created_by.presence
+			course.created_by.id == current_user.id if current_user
+		end
 	end
 end
