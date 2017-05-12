88397f6af04245cb6b843d7e7b30689fbd391626
Joshua Orozco <s3485376@student.rmit.edu.au>
2017-05-12 07:42:00 +0000

    Initial commit on testing

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "valid courses can be saved" do
+    course = courses(:one)
+    assert course.save
+  end
+  
+  test "course with no location or category can still be saved" do
+    course = courses(:two)
+    assert course.save
+  end
+  
+  test "course with short name cannot be saved" do
+    course = courses(:three)
+    assert_not course.save
+  end
+  
+  test "course with short description cannot be saved" do
+    course = courses(:four)
+    assert_not course.save
+  end
+  
+  test "course with no name cannot be saved" do
+    course = courses(:five)
+    assert_not course.save
+  end
+  
+  test "course with short prerequisite cannot be saved" do
+    course = courses(:six)
+    assert_not course.save
+  end
+  
+  test "course with no description cannot be saved" do
+    course = courses(:seven)
+    assert_not course.save
+  end
+  
+  test "course with no prerequisite cannot be saved" do
+    course = courses(:eight)
+    assert_not course.save
+  end
+  
+  
+  
+  
+  
 end
