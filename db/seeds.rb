cc4a285d2fd20dacd6344b1b49a67ae04482f908
Joshua Orozco <s3485376@student.rmit.edu.au>
2017-05-21 16:22:16 +0000

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,70 @@
 
 # disable password validation for admin
 User.create(name: "admin", email: "admin@sciencecourses.rmit.edu.au", password: "password", role: "admin").on(validate: false)
-
 User.create([
     {
         name: "Joshua Orozco",
         email: "joshua.orozco@rmit.edu.au",
         password: "123qweASD!@#",
         role: nil
+    },
+    {
+        name: "Joshy Borozky",
+        email: "joshy.borozky@rmit.edu.au",
+        password: "123qweASD!@#",
+        role: nil
     }
 ])
+
+Category.create([
+    { name: "Java" },
+    { name: "Web Programming" },
+    { name: "IT" },
+    { name: "Java Programming" },
+    { name: "iOS Development" }
+])
+
+Location.create([
+    { at: "14.09.23" },
+    { at: "96.01.02" },
+    { at: "80.07.02" }
+])
+
+Course.create([
+    {
+        name: "Building IT Systems",
+        description: "IT today is everywhere and is involved in many aspects of life, including mobile devices, electronic voting and robotic fish. Many advances have come from people passionately following an inspiration, and developing it into a concrete outcome. This course is an opportunity for you to pursue an idea in IT and to work with a team of like-minded people to develop it. You will be provided with assistance to choose an appropriate topic, find a project team, generate a project plan, and to implement your project. This course is designed to engage and inspire you with the wide world of IT, with a particular emphasis on finding appropriate tools, methodologies and techniques to meet your own personal challenge. This course will give you substantial hands-on, practical learning experiences and aims to motivate you through engagement in the creative, explorative and meaningful development of technological artefacts that operate in real world contexts.",
+        prerequisite: "Basic computer literacy",
+    },
+    {
+        name: "Introduction to Programming",
+        description: "Programming is acknowledged as a vital skill that enables problem solving through the use of computers across a range of varied disciplines. This course introduces you to basic concepts, syntax and control structures in programming. You will learn how to program in a step-wise problem solving fashion. ",
+        prerequisite: "Basic computer literacy"
+    },
+    {
+        name: "Programming 1",
+        description: "This course introduces Object-Oriented programming using the Java programming language. This course covers: algorithm development using standard control structures; design methods such as step-wise refinement and object oriented programming; the use of standard Java classes and interfaces; the use of standard APIs; I/O processing; programming style and code reuse, and basic strategies for software testing.",
+        prerequisite: "Introduction to Programming"
+    },
+    {
+        name: "Web Programming",
+        description: "The course introduces you to the basic concepts of the World Wide Web (Web), and the principles and tools that are used to develop Web applications. The course will provide an overview of Internet technology and will introduce you to current Web protocols, client side and server side programming, communication and design.",
+        prerequisite: "Introduction to Programming"
+    },
+    {
+        name: "Software Engineering Fundamentals",
+        description: "This course is designed to provide you opportunity to gain knowledge and skills necessary to analyse, design and implement complex software engineering projects. You should learn to analyse and design fairly complex real-life systems, working as teams. The project based approach used requires you to review and refine your design iteratively based on regular feedback from staff. You are also made aware of current software engineering standards and processes. You are also taught to consider qualitative aspects including maintainability, extensibility, reusability and robustness in every stage of the software-engineering life-cycle. At the end of the course you should be able to combine top-down and bottom-up approaches to software design and choose the most appropriate process considering the underlying technology, project duration, the level of risks and the customer expectations.",
+        prerequisite: "Programming 1"
+    },
+    {
+        name: "Security in Computing and Information Technology",
+        description: "Security has become a focal area in IT; it has grown and evolved significantly in recent years and different areas of specialisation have emerged.",
+        prerequisite: "Data Communication and Net-centric Computing"
+    },
+    {
+        name: "Software Engineering Project Management",
+        description: "ISYS1108 (UG): This course serves to introduce the basic principles of Engineering Software Projects. Most, if not all, students complete projects as part of assignments in various courses undertaken. These projects range in size, subject and complexity but there are basic project essentials that need to be understood and practiced for successful team project outcomes.",
+        prerequisite: "Software Engineering Fundamentals"
+    }
+])
+
